3dfe9bcdc2f21d7ddd84f5acb0dcc2e751e52e12
Colin.Heinzer <colin.heinzer@gmail.com>
2019-06-27 15:22:12 +0200

    first attempt to access the API

---
+++ lolmasterylookup/src/Components/API/Background.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import App from '../../App';
+
+class Background extends Component {
+    constructor() {
+        super();
+        this.state = {
+            masteries: [],
+    };
+}
+
+componentDidMount(){
+    fetch('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/PsychoCooki?api_key=RGAPI-c60084c5-dd91-4a58-bf8f-d369e3329941')
+    .then(results => {
+        return results.json();
+    }).then(data =>{
+        this.setState({masteries: data});
+        console.log("state", this.state.data)
+    })
+    
+    
+}
+
+
+render(){
+    return(
+        <div className="Container">
+            <div className="Container2">
+                {this.state.summoner}
+            </div>
+        </div>
+    )
+}
+
+
+}
+export default Background;
